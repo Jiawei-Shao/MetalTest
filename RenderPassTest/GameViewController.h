c426bd7f226926923101f5a984ef1fd0e0ad55a7
Jiawei Shao <jiawei.shao@intel.com>
2019-04-02 08:56:46 +0800

    Initial Commit

---
+++ RenderPassTest/GameViewController.h
@@ -0,0 +1,17 @@
+//
+//  GameViewController.h
+//  RenderPassTest
+//
+//  Created by wp on 2019/4/2.
+//  Copyright © 2019 wp. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <Metal/Metal.h>
+#import <MetalKit/MetalKit.h>
+#import "Renderer.h"
+
+// Our macOS view controller.
+@interface GameViewController : NSViewController
+
+@end
---
+++ RenderPassTest/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  RenderPassTest
+//
+//  Created by wp on 2019/4/2.
+//  Copyright © 2019 wp. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
